45f4c75c703ae2dc93f5ccfe6fd06b6aa641ac93
ritikjee <ritikj.ug21.ee@nitp.ac.in>
2024-11-14 14:56:02 +0530

    project setup

---
+++ server/app_service/src/test/java/tech/lampstack/app_service/AppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.lampstack.app_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ server/token_service/src/test/java/tech/lampstack/token_service/TokenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.lampstack.token_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
